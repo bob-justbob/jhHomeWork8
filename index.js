b914a718dd5128378ecd437d6ed6150dcb88c57e
bob-justbob <levan.qiqodze-nasidze14@geolab.edu.ge>
2021-12-19 20:56:37 +0400

    doing homework

---
+++ index.js
@@ -0,0 +1,8 @@
+import rame from "./module.js"
+
+console.log(rame.sum(5, 5))
+console.log(rame.mult(2, 5))
+
+
+import moment from 'moment';
+console.log(moment().format('DD-mm-yyyy'))
---
+++ module.js
@@ -0,0 +1,12 @@
+function sum(x, y) {
+    return x + y;
+};
+
+function mult(x, y) {
+    return x * y
+}
+
+export default {
+    sum,
+    mult
+}
